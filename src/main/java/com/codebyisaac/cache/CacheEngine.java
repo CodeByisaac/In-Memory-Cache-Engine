f90994f7c430a3c1575c3f70a6f5a0ea75fc6b27
CodeByisaac <highzik86@gmail.com>
2026-06-09 19:33:06 -0400

    Add HashMap for storage W/ GET SET DELETE commands

--- src/main/java/com/codebyisaac/cache/CacheEngine.java
+++ src/main/java/com/codebyisaac/cache/CacheEngine.java
@@ -1,6 +1,18 @@
-// storage for data
-// package com.codebyisaac.cache;
+//storage for data (HashMap<key,value>)
+package com.codebyisaac.cache;
+import java.util.Map;
+import java.util.HashMap;
 
-// public class CacheEngine {
-    
-// }
+public class CacheEngine {
+    private final Map<String,String> memoryMap = new HashMap<>();
+
+    public void set(String key, String value){
+        memoryMap.put(key,value);
+    }
+    public String get(String key) {
+        return memoryMap.get(key);
+    }
+    public void delete(String key){
+        memoryMap.remove(key);
+    }
+}
