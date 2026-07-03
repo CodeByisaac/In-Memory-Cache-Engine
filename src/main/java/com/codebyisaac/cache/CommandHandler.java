4388d7fc35055a9bdc97fd8b21cb9ad0183129b7
CodeByisaac <highzik86@gmail.com>
2026-07-03 02:34:32 -0400

    update parser to accept TTL in millis

--- src/main/java/com/codebyisaac/cache/CommandHandler.java
+++ src/main/java/com/codebyisaac/cache/CommandHandler.java
@@ -2,10 +2,10 @@ package com.codebyisaac.cache;
 
 import java.util.List;
 public class CommandHandler {
-    private final CacheStorage cache;
+    private final CacheStorage storage;
 
-    public CommandHandler(CacheStorage cache) {
-        this.cache = cache;
+    public CommandHandler(CacheStorage storage) {
+        this.storage = storage;
     }
 
     public String execute(List<String>tokens) {
@@ -21,22 +21,29 @@ public class CommandHandler {
                 
             case "SET":
                 if (tokens.size() < 3) return ProtocolParser.toError("ERR wrong no of argumnents for 'set' command");
-                String setKey = tokens.get(1);
-                String setValue = tokens.get(2);
-                cache.set(setKey, setValue);
+                String key = tokens.get(1);
+                String val = tokens.get(2);
+                
+                //check if client passed : SET key value PX 5000
+                if (tokens.size() >= 5 && tokens.get(3).equalsIgnoreCase("PX")){
+                    long ttl = Long.parseLong(tokens.get(4));
+                    storage.set(key, val, ttl);
+                } else {
+                    storage.set(key, val);
+                }
                 return ProtocolParser.toString("OK");
 
             case "GET":
                 if (tokens.size() < 2) return ProtocolParser.toError("ERR wrong no of argument for 'get' command ");
                 String getKey = tokens.get(1);
-                String result = cache.get(getKey);
+                String result = storage.get(getKey);
                 if (result == null) return ProtocolParser.toNullBulkString();
                 return ProtocolParser.toBulkString(result);
 
             case "DELETE":
                 if (tokens.size() < 2) return ProtocolParser.toError("ERR wrong no of argument for 'delete' command");
                 String delKey = tokens.get(1);
-                cache.delete(delKey);
+                storage.delete(delKey);
                 return ProtocolParser.toString("OK");
         
             default:
